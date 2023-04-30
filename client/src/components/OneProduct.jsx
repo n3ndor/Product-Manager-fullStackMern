ef934f4b81786040c253659dbb75eed326e240c3
n3ndor <n3ndor@gmail.com>
2023-04-30 11:03:25 -0400

    CRUD ready, added navigation buttons

--- client/src/components/OneProduct.jsx
+++ client/src/components/OneProduct.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, Link } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const OneProduct = (props) => {
 
     const { id } = useParams();
     const [OneProduct, setOneProduct] = useState({});
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios
@@ -18,12 +19,22 @@ const OneProduct = (props) => {
             .catch((err) => console.log(err))
     }, [id])
 
+    const deleteProduct = () => {
+        axios.delete("http://localhost:8000/api/products/" + id)
+            .then(res => {
+                console.log(res.data)
+                navigate("/")
+            })
+            .catch(err => console.log(err))
+    }
+
 
     return (
         <div>
             <h2>{OneProduct.title}</h2>
             <p>Price: {OneProduct.price}</p>
             <p>Description: {OneProduct.description}</p>
+            <button onClick={deleteProduct}>Delete</button>
             <Link to={"/"}>Go Back</Link>
         </div>
     )
